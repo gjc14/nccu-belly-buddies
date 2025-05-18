9fa86ec76cc7592ce6cc71523774a2fb321cc0bc
yunsin0408 <yunsin.hung@gmail.com>
2025-05-18 21:17:37 +0800

    new schema

---
+++ app/lib/db/schema/restaurant.ts
@@ -0,0 +1,13 @@
+import {  pgTable, uuid, text, integer} from 'drizzle-orm/pg-core'
+
+export const restaurant = pgTable('restaurant', {
+    id: uuid('res_id').defaultRandom().primaryKey(),
+    name: text('res_name').notNull(),
+    description: text('res_description'),
+    address: text('address').notNull(),
+    phone: text('phone'),
+    openingHours: text('opening_hours'),
+    cuisineType: text('cuisine_type'),
+    priceRange: text('price_range'),
+    rating: integer('rating'),
+})
