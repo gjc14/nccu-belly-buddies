6a0ba795b84fc3f7fb0b4084a9d423d2367b8d99
TingKai <112306077@g.nccu.edu.tw>
2025-05-23 21:03:04 +0800

    add v0 UI

--- app/routes/web/hello-world/index/route.tsx
+++ app/routes/web/hello-world/index/route.tsx
@@ -1,13 +1,18 @@
+
+import { AppShell } from "../components/app-shell"
+import { RoomList } from "../components/room-list"
+
 export async function loader() {
 	return null
 }
 
 export default function Index() {
 	return (
-		<div className="border-2">
-			<p>
-				This content is from <strong>index.tsx</strong> file.
-			</p>
-		</div>
+<AppShell>
+      <div className="container py-6">
+        <h1 className="text-3xl font-bold mb-6">Available Rooms</h1>
+        <RoomList />
+      </div>
+    </AppShell>
 	)
 }
