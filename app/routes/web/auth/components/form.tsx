cbee2eea1d259180994e140ad1e6ea774c3fd76a
gjc14 <yin@yinc.me>
2025-05-23 23:39:33 +0800

    feat: login form with otp available

--- app/routes/web/auth/components/form.tsx
+++ app/routes/web/auth/components/form.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Form } from 'react-router'
 
 import { Button } from '~/components/ui/button'
@@ -8,43 +9,86 @@ import { cn } from '~/lib/utils'
 
 export function LoginForm({
 	onLogin,
+	showOtpInput,
+	onVerifyOtp,
 	className,
 	...props
 }: React.ComponentProps<'div'> & {
 	onLogin: (email: string) => void
+	showOtpInput: boolean
+	onVerifyOtp: (email: string, otp: string) => void
 }) {
+	const [email, setEmail] = useState('')
+
 	return (
 		<div className={cn('flex flex-col gap-6', className)} {...props}>
+			<div className="relative size-32 md:hidden bg-muted mx-auto">
+				<img
+					src="/logo.png"
+					alt="Belly Buddy"
+					className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+				/>
+			</div>
 			<Card className="overflow-hidden">
 				<CardContent className="grid p-0 md:grid-cols-2">
-					<Form
-						className="px-8 py-12 lg:px-12 lg:py-20"
-						onSubmit={e => {
-							e.preventDefault()
-							onLogin(e.currentTarget.email.value)
-						}}
-					>
-						<div className="flex flex-col gap-6">
-							<div className="flex flex-col items-center text-center">
-								<h1 className="text-2xl font-bold">歡迎使用 Belly Buddy</h1>
-								<p className="text-balance text-muted-foreground">
-									立即輸入你的 Email 以登入或註冊，找到你的 Belly Buddy。
-								</p>
+					{!showOtpInput ? (
+						<Form
+							key={'login-form'}
+							className="px-8 py-12 lg:px-12 lg:py-20"
+							onSubmit={e => {
+								e.preventDefault()
+								onLogin(email)
+							}}
+						>
+							<div className="flex flex-col gap-6">
+								<div className="flex flex-col items-center text-center">
+									<h1 className="text-2xl font-bold">歡迎使用 Belly Buddy</h1>
+									<p className="text-balance text-muted-foreground">
+										立即輸入你的 Email 以登入或註冊，找到你的 Belly Buddy。
+									</p>
+								</div>
+								<div className="grid gap-2">
+									<Label htmlFor="email">Email</Label>
+									<Input
+										id="email"
+										type="email"
+										value={email}
+										onChange={e => setEmail(e.target.value)}
+										placeholder="m@example.com"
+										required
+									/>
+								</div>
+								<Button type="submit" className="w-full">
+									使用 Email 繼續
+								</Button>
 							</div>
-							<div className="grid gap-2">
-								<Label htmlFor="email">Email</Label>
-								<Input
-									id="email"
-									type="email"
-									placeholder="m@example.com"
-									required
-								/>
+						</Form>
+					) : (
+						<Form
+							key={'otp-form'}
+							className="px-8 py-12 lg:px-12 lg:py-20"
+							onSubmit={e => {
+								e.preventDefault()
+								onVerifyOtp(email, e.currentTarget.otp.value)
+							}}
+						>
+							<div className="flex flex-col gap-6">
+								<div className="flex flex-col items-center text-center">
+									<h1 className="text-2xl font-bold">歡迎使用 Belly Buddy</h1>
+									<p className="text-balance text-muted-foreground">
+										請輸入您收到的 OTP 以登入或註冊，找到你的 Belly Buddy。
+									</p>
+								</div>
+								<div className="grid gap-2">
+									<Label htmlFor="otp">輸入 OTP 一次性驗證碼</Label>
+									<Input id="otp" type="otp" placeholder="123456" required />
+								</div>
+								<Button type="submit" className="w-full">
+									登入
+								</Button>
 							</div>
-							<Button type="submit" className="w-full">
-								使用 Email 繼續
-							</Button>
-						</div>
-					</Form>
+						</Form>
+					)}
 					<div className="relative hidden bg-muted md:block">
 						<img
 							src="/logo.png"
